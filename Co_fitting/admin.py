40a03c705cabea683a45057ed19ea00c805a9ac4
Hiruge-local <okita1110@outlook.com>
2025-01-24 19:10:32 +0900

    [update] レシピテーブルの様子を確認できるように、管理画面の表示を変更

--- Co_fitting/admin.py
+++ Co_fitting/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import CustomUser
+from Co_fitting.models import Recipe, RecipeStep
 
 
 @admin.register(CustomUser)
@@ -9,3 +10,19 @@ class CustomUserAdmin(UserAdmin):
         ('Custom Fields', {'fields': ('preset_limit',)}),
     )
     list_display = ('username', 'email', 'is_staff', 'preset_limit')
+
+
+class RecipeStepInline(admin.TabularInline):  # RecipeStepをRecipeの詳細ページにインラインで表示するための設定
+    model = RecipeStep
+    extra = 1  # 新しいRecipeStepを追加するための空白フォームの数
+    fields = ('step_number', 'water_ml_this_step', 'minute', 'seconds')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'is_ice', 'len_steps', 'bean_g', 'water_ml', 'memo')
+    list_filter = ('is_ice',)  # アイスかどうかでフィルタリングできるようにする
+    search_fields = ('name',)  # レシピ名で検索可能にする
+
+    # Recipeの詳細ページにRecipeStepをインラインで表示
+    inlines = [RecipeStepInline]
