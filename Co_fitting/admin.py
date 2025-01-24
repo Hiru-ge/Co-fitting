95ed4ca3de74b740925a3ef5dc540d2bbed62af7
Hiruge-local <okita1110@outlook.com>
2025-01-24 17:58:16 +0900

    [update] rootユーザー作成、管理画面の表示項目を編集

--- Co_fitting/admin.py
+++ Co_fitting/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('preset_limit',)}),
+    )
+    list_display = ('username', 'email', 'is_staff', 'preset_limit')
