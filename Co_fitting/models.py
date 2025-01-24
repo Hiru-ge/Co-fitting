916f0946ad3f2b594cbbd14b94a2e25836517dcd
Hiruge-local <okita1110@outlook.com>
2025-01-24 19:08:49 +0900

    [update] MyPreset登録用のテーブルを追加・マイグレーション

--- Co_fitting/models.py
+++ Co_fitting/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=30)
+    create_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    is_ice = models.BooleanField(default=False)
+    len_steps = models.IntegerField()
+    bean_g = models.FloatField()
+    water_ml = models.FloatField()
+    memo = models.TextField(blank=True, null=True, max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeStep(models.Model):
+    recipe_id = models.ForeignKey(to=Recipe, on_delete=models.CASCADE, related_name='steps')
+    step_number = models.IntegerField()
+    minute = models.IntegerField()
+    seconds = models.IntegerField()
+    water_ml_this_step = models.FloatField()
+
+    class Meta:
+        ordering = ['step_number']  # 手順の順番で並べる
+
+    def __str__(self):
+        return f"Step {self.step_number} for {self.recipe_id.name}"
