a6262fb750ff330d466d8d75f325bad4a67a00fa
Hiruge-local <okita1110@outlook.com>
2025-02-13 21:15:09 +0900

    [update] SECRET_KEYを.env内に格納
    django-environをpip installして、環境変数としてSECRET_KEYを登録した
    こんな便利なものがあるのならもっと早くやっておけば良かった

--- Co_fitting/settings.py
+++ Co_fitting/settings.py
@@ -12,16 +12,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 環境変数の読み込み
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
