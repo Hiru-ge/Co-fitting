c6c516c3fd50dbc4d732c27f68447f80199b9395
Hiruge-local <okita1110@outlook.com>
2025-02-14 15:21:27 +0900

    [update] DBにMySQLを使うように設定
    高いアクセス負荷にも耐えられるように、DBをMySQLに変更

--- Co_fitting/settings.py
+++ Co_fitting/settings.py
@@ -94,8 +94,15 @@ LOGOUT_REDIRECT_URL = '/users/login'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Co_fitting',
+        'USER': 'root',
+        'PASSWORD': env('DATABASE_KEY'),
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
