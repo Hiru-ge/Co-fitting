8f3a5cf13b457758043d11a86906d256bcc89c0a
Hiruge_local <okita1110@outlook.com>
2025-03-19 11:34:22 +0900

    [update] settings内の環境変数の追加

--- Co_fitting/settings.py
+++ Co_fitting/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_extensions',
+    env('IS_DJANGO_EXTENSION'),
     'recipes',
     'users',
     'purchase',
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/var/www/html/Co_fitting/static/"
+STATIC_ROOT = env('STATIC_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
