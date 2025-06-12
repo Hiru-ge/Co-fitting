6abbf394c172adbd847a277c0997bd044ce2c089
Hiruge_local <okita1110@outlook.com>
2025-06-12 16:54:27 +0900

    Revert "[update] UI作成・モデル定義"
    This reverts commit 0918314ba8982d12cd3cdb344693b42baa74e0d5.
    作りかけのレシピ共有機能を間違ってコミットしてしまったのでRevert！

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from users.models import User
-from django.utils.crypto import get_random_string
-from django.utils import timezone
-from datetime import timedelta
 
 
 class Recipe(models.Model):
@@ -31,38 +28,3 @@ class RecipeStep(models.Model):
 
     def __str__(self):
         return f"Step {self.step_number} for {self.recipe_id.name}"
-
-
-class SharedRecipe(models.Model):
-    name = models.CharField(max_length=30)
-    shared_by_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    is_ice = models.BooleanField(default=False)
-    ice_g = models.FloatField(null=True, blank=True)
-    len_steps = models.IntegerField()
-    bean_g = models.FloatField()
-    water_ml = models.FloatField()
-    memo = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField(null=True)
-    access_token = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.access_token:
-            self.access_token = get_random_string(32)
-        if not self.expires_at:
-            self.expires_at = timezone.now() + timedelta(days=30)
-        super().save(*args, **kwargs)
-
-
-class SharedRecipeStep(models.Model):
-    shared_recipe = models.ForeignKey(SharedRecipe, on_delete=models.CASCADE)
-    step_number = models.IntegerField()
-    minute = models.IntegerField()
-    seconds = models.IntegerField()
-    total_water_ml_this_step = models.FloatField()
-
-    def __str__(self):
-        return f"{self.shared_recipe.name} - Step {self.step_number}"
--- recipes/urls.py
+++ recipes/urls.py
@@ -10,7 +10,4 @@ urlpatterns = [
     path('preset_create', views.preset_create, name='preset_create'),
     path('preset_edit/<recipe_id>', views.preset_edit, name='preset_edit'),
     path('preset_delete/<int:pk>', PresetDeleteView.as_view(), name='preset_delete'),
-    path('share/', views.share_recipe, name='share_recipe'),
-    path('shared/<str:access_token>/', views.shared_recipe, name='shared_recipe'),
-    path('shared/<str:access_token>/add/', views.add_shared_recipe, name='add_shared_recipe'),
 ]
