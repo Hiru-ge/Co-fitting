09968f137ad133e8d2387e0e0ec9d304d98fab68
hiruge <okita0249@gmail.com>
2025-09-21 15:49:45 +0900

    [hotfix] 先のリファクタリングで誤って削除してしまったルーティングを復活

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,7 @@ from .views import PresetDeleteView
 app_name = 'recipes'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('mypage', views.mypage, name='mypage'),
     path('preset_create', views.preset_create, name='preset_create'),
     path('preset_edit/<recipe_id>', views.preset_edit, name='preset_edit'),
